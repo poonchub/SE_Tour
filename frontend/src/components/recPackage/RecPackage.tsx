606a0c778a47c698146f2cbc67e088b76f491aee
poonchub <141898614+poonchub@users.noreply.github.com>
2024-12-03 01:27:47 +0700

    update

--- frontend/src/components/recPackage/RecPackage.tsx
+++ frontend/src/components/recPackage/RecPackage.tsx
@@ -1,16 +1,21 @@
+import { Link } from "react-router-dom"
 import "./RecPackage.css"
 
-function RecPackage(){
+function RecPackage() {
     return (
         <div className="rec-package-container">
-            <div className="title">
-                <div className="icon-box">
-                    
+            <div className="card-title">
+                <div className="title">
+                    <div className="icon-box">
+                        <img src="/images/icons/island.png" alt="" />
+                    </div>
+                    <div className="text-box">แนะนำแพ็กเกจทัวร์สุดพิเศษ</div>
                 </div>
-                <div className="text-box">
-                    
-                </div>
+                <Link to={"/tour-package"}>
+                    <button className="more-tour-btn">แพ็กเกจทั้งหมด</button>
+                </Link>
             </div>
+
         </div>
     )
 }
--- frontend/src/components/showTourSlice/ShowTourSlice.tsx
+++ frontend/src/components/showTourSlice/ShowTourSlice.tsx
@@ -7,9 +7,9 @@ import { Carousel } from "antd";
 function ShowTourSlice() {
     const [tourPackages, setTourPackages] = useState<TourPackagesInterface[]>([]);
 
-    const [imageBg, setimageBg] = useState()
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [startPriceFormat, setStartPriceFormat] = useState<string>("");
 
     async function getTourPackages() {
         let res = await GetTourPackages()
@@ -28,16 +28,45 @@ function ShowTourSlice() {
         }
     }
 
+    function handleSetStartPrice() {
+        let startPrice = 999999;
+        tourPackages[currentSlide]?.TourPrices?.forEach((price: any) => {
+            if (price.PersonTypeID !== 1 && price.Price && price.Price < startPrice) {
+                startPrice = price.Price;
+            }
+        });
+        setStartPriceFormat(startPrice.toLocaleString('th-TH', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }))
+    }
+
+    function setPackageData(id: number | undefined) {
+        localStorage.setItem("startPrice", startPriceFormat)
+        localStorage.setItem("tourPackageID", String(id))
+        setTimeout(() => {
+            location.href = "/tour-select";
+        });
+    }
+
+    console.log(startPriceFormat)
+
+    useEffect(() => {
+        handleSetStartPrice()
+    }, [currentSlide, tourPackages]);
+
     useEffect(() => {
         fetchData()
     }, [])
 
     console.log(tourPackages?.[0]?.TourDescriptions?.Intro)
 
+    const bigImageUrl = `${apiUrl}/${tourPackages[currentSlide]?.TourImages?.[0]?.FilePath}`
+
     const imageElement = tourPackages.map((tour, index) => {
         const imageUrl = `${apiUrl}/${tour?.TourImages?.[0]?.FilePath}`
         return (
-            <div className="image-box" key={index}>
+            <div className="image-box" key={index} onClick={()=>setPackageData(tour.ID)}>
                 <img src={imageUrl} alt="" />
             </div>
         )
@@ -46,7 +75,7 @@ function ShowTourSlice() {
     return (
         <div className="show-tour-slice-container">
             <div className="image-bg">
-                <img src={"/images/backgrounds/background.jpg"} alt="" />
+                <img src={bigImageUrl} alt="" />
             </div>
             <div className="tour-container">
                 <div className="image-box">
